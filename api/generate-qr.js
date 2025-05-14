d7361636056ea02eec41295c3d1152bb646b9a08
Azan Khan <azankhan4934465@gmail.com>
2025-05-14 23:14:40 +1000

    Fixed routing and configuration

--- api/generate-qr.js
+++ api/generate-qr.js
@@ -1,16 +1,15 @@
 const QRCode = require('qrcode');
 
-module.exports = async (req, res) => {
-  const { url } = req.query;
-
+module.exports = (req, res) => {
+  const url = req.query.url;
   if (!url) {
     return res.status(400).json({ error: 'URL is required' });
   }
 
-  try {
-    const qrCode = await QRCode.toDataURL(url);
-    res.status(200).json({ qr_code: qrCode.split(',')[1] });
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to generate QR code' });
-  }
+  QRCode.toDataURL(url, (err, qrCode) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to generate QR code' });
+    }
+    res.status(200).json({ qr_code: qrCode.split(',')[1] }); // Send base64 image data
+  });
 };
